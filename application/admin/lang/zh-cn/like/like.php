898bf82d7a61b8fda1966f9a661e7284404f8585
1433347057@qq.com <1433347057@qq.com>
2023-05-15 17:27:11 +0800

    这是第一次提交

---
+++ application/admin/lang/zh-cn/like/like.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Id'         => 'ID',
+    'User_id'    => '点赞用户id',
+    'Like_type'  => '点赞类型  1资讯 2动态 3评论',
+    'Parent_id'  => '被点赞目标id',
+    'Createtime' => '点赞时间'
+];
---
+++ application/common/model/like/Like.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace app\common\model\like;
+
+use app\common\model\Comment\Comment;
+use app\common\model\Dynamic\Dynamic;
+use app\common\model\information\Information;
+use think\Db;
+use think\Exception;
+use think\Model;
+
+
+class Like extends Model
+{
+
+    
+
+    
+
+    // 表名
+    protected $name = 'like';
+    
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = 'integer';
+
+    // 定义时间戳字段名
+    protected $createTime = 'createtime';
+    protected $updateTime = false;
+    protected $deleteTime = false;
+
+    // 追加属性
+    protected $append = [
+
+    ];
+
+    /**增加点赞数
+     * @param $user_id
+     * @param $parent_id
+     * @param $like_type
+     * @return bool
+     */
+    public function incLikes($user_id='', $parent_id="", $like_type)
+    {
+        if (empty($user_id)){
+            return $this->error("用户id参数错误",[],400,"json");
+        }
+        if (empty($parent_id)){
+            return $this->error("id参数错误",[],400,"json");
+        }
+        $like=new self;
+        if ($like_type==1){
+            Db::startTrans();
+            try {
+                $count=$like::where('user_id',"=",$user_id)
+                    ->where("parent_id",'=',$parent_id)
+                    ->where("like_type",'=',$like_type)
+                    ->count();
+                if ($count){
+                    return $this->error("请勿重复点赞",[],204,"json");
+                }else{
+                    Information::where("id","=",$parent_id)
+                        ->setInc('likes');
+                }
+                $data=[
+                    "user_id"=>$user_id,
+                    "parent_id"=>$parent_id,
+                    "like_type"=>$like_type,
+                ];
+                $like::create($data);
+                Db::commit();
+                return true;
+            }catch (Exception $e){
+                Db::rollback();
+                return false;
+            }
+
+        }else if ($like_type==2){
+            Db::startTrans();
+            try {
+                $count=$like::where('user_id',"=",$user_id)
+                    ->where("parent_id",'=',$parent_id)
+                    ->where("like_type",'=',$like_type)
+                    ->count();
+                if ($count){
+                    return $this->error("请勿重复点赞",[],204,"json");
+                }else{
+                    Dynamic::where("id","=",$parent_id)
+                        ->setInc('likes');
+                }
+
+                $data=[
+                    "user_id"=>$user_id,
+                    "parent_id"=>$parent_id,
+                    "like_type"=>$like_type,
+                ];
+                $like::create($data);
+                Db::commit();
+                return true;
+            }catch (Exception $e){
+                Db::rollback();
+                return false;
+            }
+        }else if ($like_type==3){
+            Db::startTrans();
+            try {
+                $count=$like::where('user_id',"=",$user_id)
+                    ->where("parent_id",'=',$parent_id)
+                    ->where("like_type",'=',$like_type)
+                    ->count();
+                if ($count){
+                    return $this->error("请勿重复点赞",[],204,"json");
+                }else{
+                    Comment::where("id","=",$parent_id)
+                        ->setInc('likes');
+                }
+                $data=[
+                    "user_id"=>$user_id,
+                    "parent_id"=>$parent_id,
+                    "like_type"=>$like_type,
+                ];
+                $like::create($data);
+                Db::commit();
+                return true;
+            }catch (Exception $e){
+                Db::rollback();
+                return false;
+            }
+        }else{
+            return false;
+        }
+
+    }
+
+
+}
